3cd645b03f6062d7c87671a50328ce3f90e50896
Nkosinathi825 <nkosinathiphiri825@gmail.com>
2024-09-02 12:07:57 +0000

    done with the upper part

--- mytodolist-app/src/components/AddTask.js
+++ mytodolist-app/src/components/AddTask.js
@@ -8,12 +8,14 @@ function AddTask() {
     <section className="AddTaskWrapper">
         <form  className='form'>
             <section className='inputs'>
-              <label>Title</label>
-              <input type="text" placeholder='Write your todo title'  value={input} className='inputTitle' />
-            </section>
-            <section className='inputs'>
-              <label>Description</label>
-              <input type="text" placeholder='Write your todo description' value={input}  className='inputDescription' />
+              <section className='Labes'>
+                <label>Title</label>
+                <label>Description</label>
+              </section>
+              <section className='input'>
+                <input type="text" placeholder='Write your todo title'  value={input} className='inputTitle' />
+                <input type="text" placeholder='Write your todo description' value={input}  className='inputDescription' />
+              </section>
             </section>
             <section className='button'>
               <button className='submit'>Add</button>
@@ -21,8 +23,8 @@ function AddTask() {
            
         </form>
         <section className='viewTable'>
-            <button>Todo List </button>
-            <button>Completed List</button>
+            <button className='todolist'>Todo List </button>
+            <button className='completed'>Completed List</button>
         </section>
     </section>
   );
